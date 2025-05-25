79e4b9f6b07c59a8f2ee5288823ef7ab4a69ecf5
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-05-25 21:00:30 +0000

    index css js pages image

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Peter Zerbinos All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates division using repeated subtraction.
+ */
+function calculateDivisionWithSubtraction() {
+  // variables
+  const userDividend = parseInt(document.getElementById('user-dividend').value)
+  const userDivisor = parseInt(document.getElementById('user-divisor').value)
+  let remainder = userDividend
+  let counter = 0
+
+  // process
+  while (true) {
+    counter++
+    remainder -= userDivisor
+    if (remainder < userDivisor) {
+      break
+    }
+  }
+
+  // output
+  document.getElementById('result').innerText =
+    userDividend + ' ÷ ' + userDivisor + ' = ' + counter + ' and the remainder is: ' + remainder
+}
